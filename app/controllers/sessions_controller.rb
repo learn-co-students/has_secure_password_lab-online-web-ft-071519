a006652a1bea1c06c00015bef13bb3ec8915d05b
Audrea Cook <audreacwork@gmail.com>
2019-10-02 14:11:40 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:name])
     return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
+    redirect_to root_path
   end
 
   def new
@@ -10,6 +11,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete :user_id
+    redirect_to login_path
   end
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def home
-
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    else
+      redirect_to login_path
+    end
   end
 
   def new
@@ -9,11 +13,11 @@ class UsersController < ApplicationController
 
   def create
     if params[:password] != params[:password_confirmation]
-      redirect_to new_user_path
+      render new_user_path
       else
         @user = User.create(user_params)
         session[:user_id] = @user.id
-        redirect_to '/'
+        redirect_to root_path
     end
   end
 
